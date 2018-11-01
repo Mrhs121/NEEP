6a77a92a2c16513893de83844e13697835293d0c
Sheng Huang <1367467330@qq.com>
2018-11-01 08:12:22 +0800

    fix a bug equation

--- zhenti/2011.c
+++ zhenti/2011.c
@@ -69,6 +69,7 @@ int checkBit(int x,int N)
     return 0;
 }
 
+// 计算不含101串的个数
 int test101()
 {
   
@@ -109,10 +110,80 @@ void  testMaxBlock()
     }
     int max = maxBlock(p,5,10);
 }
+
+float print(int arr[],int n){
+    int i=0;
+    float sum = 0;
+    for(i=0;i<n;i++){
+       printf("%d ",arr[i]);
+        sum += arr[i];
+    }
+    printf("\n");
+    return sum;
+}
+// 例如表达式 100a+10=4a-2+123-22a char express[] = "100a+10=4a-2+123-22a";
+// 思路：将每个参数全部提取出来100a 10 -4a 2 -123 22a
+// 然后将纯数字放入一个数组中
+// 变量放在另外一个数组中
+double equation(char express[]){
+    int rightOrleft = 1;
+    int next = 1;
+    int i=0,m=0,n=0;
+    char c = express[i];
+    int var[10]; // 存放变量前面的系数
+    int para[10]; // 存放实数
+    int top = 0;
+    char temp[10];
+    if(express[0]=='-'){
+        next = -1;
+        c = express[++i];
+    }
+    
+    while (c!='\0') {
+        if( (c<='9' && c>='0') || (c<='z' && c>='a')){
+            //将用于计算的变量或者纯数字缓存起来
+            temp[top++] = c;
+            temp[top] = '\0';//用于后面的字符串转数字
+        } else {
+            // 处理加减符号
+            top--; //除去末尾的'\0'
+            if(temp[top]<='z' && temp[top]>='a'){
+                //如果缓存的是变量 1a 3a 100a -1a 末尾必定是字母
+                var[m++] = rightOrleft*next*(temp[0]>='a'?1:atoi(temp));
+            } else {
+                //如果缓存的是纯数字
+                para[n++] = rightOrleft*next*(atoi(temp));
+            }
+            // 对下一个对象正负的判断
+            if(c=='-'){
+                next = -1;
+            } else if(c == '+'){
+                next = 1;
+            }
+            // 缓存清零
+            top = 0;
+        }
+        if(c=='='){
+        	next = 1;
+            rightOrleft = -1;
+        }
+        c = express[++i];
+    }
+    top--;// 除去最后一个'\0'
+    // 处理最后一个参数或者变量
+    if(temp[top]<='z' && temp[top]>='a')
+        var[m++] = rightOrleft*next*(atoi(temp));
+    else
+        para[n++] = rightOrleft*next*(atoi(temp));
+    return -print(para, n)/print(var, m);
+}
 int main()
 {
 
 	test101();
+	//char express[] = "-5a+10=4a-2+123-22a+22";
+	char express[] = "a+2a+3a-12=a+13";
+    printf("%s\na = %.5f\n",express,equation(express));
 	// unsigned char s[4];
 	// i2ba(200,s);
 	// printf("%d %d %d %d\n",s[0],s[1],s[2],s[3]);
