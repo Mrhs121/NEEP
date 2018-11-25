b75fcae986b1339fa441c34c78710eda6db8a551
Sheng Huang <1367467330@qq.com>
2018-11-25 23:05:13 +0800

    add leetcode

---
+++ leetcode/test.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// 最长不重复子串
+int lengthOfLongestSubstring(char* s) {
+    //if(ste)
+    if(strlen(s)==0)
+        return 0;
+    if(strlen(s)==1)
+        return 1;
+    int check[127] = {0};
+    int index[127] = {0};
+    int length = 0,i = 0,max = 1,start=0,end = 0,j=0;
+    char c;
+    while( (c=s[i]) != '\0'){
+        if(check[c]==0){
+            length++;
+            index[c] = i;
+            check[c] = 1;
+        } else {
+            //printf("--\n");
+            if(max<=length){
+                max = length;
+                end = i;
+            }
+            length = length-index[c]+start;
+            for(j=start;j<index[c];j++)
+                check[s[j]] = 0;
+            start = index[c]+1; 
+            index[c] = i;
+            //check[c-97] = 0;
+        }
+        i++;
+       // printf("cur len :%d,max %d\n",length,max);
+    }
+    if(max<=length){
+        max = length;
+        end = i;
+    }
+    printf("end = %d\n",end);
+    return max;
+}
+
+int main(){
+    printf("%d\n",lengthOfLongestSubstring("aiuiothgfksanv"));
+    return 0;
+}
