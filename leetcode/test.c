43bb3c6adfa82f5bca0667ab4ad408f81737e936
Sheng Huang <1367467330@qq.com>
2018-12-02 20:36:08 +0800

    add longestPalindrome

--- leetcode/test.c
+++ leetcode/test.c
@@ -27,12 +27,12 @@ int lengthOfLongestSubstring(char* s) {
             length = length-index[c]+start;
             for(j=start;j<index[c];j++)
                 check[s[j]] = 0;
-            start = index[c]+1; 
+            start = index[c]+1;
             index[c] = i;
             //check[c-97] = 0;
         }
         i++;
-       // printf("cur len :%d,max %d\n",length,max);
+        // printf("cur len :%d,max %d\n",length,max);
     }
     if(max<=length){
         max = length;
@@ -47,60 +47,70 @@ int shoppingOffers(int* price, int priceSize, int** special, int specialRowSize,
 }
 
 
-
+char * dest;
 // 最长回文子串
-int  longestPalindrome(char* s) {
+char *  longestPalindrome(char* s) {
     int length = strlen(s);
+    printf("%d\n",length);
     int i =0 ,j =0;
-    int max = -1;
-    int maxlength = 0;
+    int start = 0;
+    int maxlength = 0,maxlength2=0;
     int final=0,finallength=0;
     // 有两种情况，单核串 双核串
     for(i=0;i<length;i++){
-
+        if(i==11){
+            
+        }
         if(s[i+1] == s[i]){
             // 双核
-            max = 2; // 保底为2
             maxlength =2;
-            for (j = 1; j<=(i<length/2)?i:length-1-i; j++)
+            int a =(i<=length/2)?i:length-1-i;
+            
+            for (j = 1; j<=a; j++)
             {
-                if(s[i-j] == s[i+i+j]){
-                    //max++;
+                if(s[i-j] == s[i+1+j]){
                     maxlength += 2;
                 }
-                else 
-                    break;   
+                else
+                    break;
             }
             //final = max>final?max:final;
-            final = maxlength>finallength?i:final;
+            start = maxlength>finallength?(i-j+1):start;
             finallength = maxlength>finallength?maxlength:finallength;
-        } else {
+        } //else {
             // 单核
             //max = 1; // 保底为2
-            maxlength =1;
-            for (j = 1; j<=(i<length/2)?i:length-1-i; j++)
+            maxlength2 =1;
+            int b =(i<=length/2)?i:length-1-i;
+            for (j = 1; j<=b; j++)
             {
                 if(s[i-j] == s[i+j]){
                     //max++;
-                    maxlength += 2;
+                    maxlength2 += 2;
                 }
-                else 
-                    break;   
+                else
+                    break;
             }
-
+        
+            maxlength = maxlength>maxlength2?maxlength:maxlength2;
+            start = maxlength>finallength?i-j+1:start;
             finallength = maxlength>finallength?maxlength:finallength;
-            final = maxlength>finallength?i:final;
-        }
-
+        //}
+         printf("finallength = %d , start = %d\n",finallength,start);
     }
-    return final;
+    printf("finallength = %d , start = %d\n",finallength,start);
+    char * dest = (char*)malloc(sizeof(char)*finallength+1);
+    dest[finallength] = '\0';
+    strncpy(dest,s+start,finallength);
+    return dest;
 }
 
 int main(){
-
-
-    int maxlength = longestPalindrome("bbcdyxabaxyda");
-    printf("Max Length = %d\n",maxlength);
+    
+    //bbcdyxabaxyda
+    
+    printf("%s\n",longestPalindrome("bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"));
+    //printf("Max Length = %d\n",maxlength);
     //printf("%d\n",lengthOfLongestSubstring("aiuiothgfksanv"));
     return 0;
 }
